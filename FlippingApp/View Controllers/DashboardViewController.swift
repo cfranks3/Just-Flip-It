b7fca535928ba1c00b575f572d26ab002c609fa4
Bronson Mullens <bronsonmullens@icloud.com>
2021-02-12 01:05:46 -0800

    Stubbed out dashboard view controller file and linked to storyboard vc.

---
+++ FlippingApp/View Controllers/DashboardViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DashboardViewController.swift
+//  FlippingApp
+//
+//  Created by Bronson Mullens on 2/12/21.
+//
+
+import UIKit
+
+class DashboardViewController: UIViewController {
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
