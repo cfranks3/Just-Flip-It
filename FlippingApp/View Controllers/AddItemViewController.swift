77f8b5fdaef3a132ce81a3bb3c7beaef3cdcaa47
Bronson Mullens <bronsonmullens@icloud.com>
2021-02-12 01:56:32 -0800

    Stubbed out add item vc and set up autolayout constraints.

--- FlippingApp/View Controllers/AddItemViewController.swift
+++ FlippingApp/View Controllers/AddItemViewController.swift
@@ -9,6 +9,23 @@ import UIKit
 
 class AddItemViewController: UIViewController {
     
+    // MARK: - Properties
+    
+    var itemController: ItemController?
+    
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var purchasePriceTextField: UITextField!
+    @IBOutlet weak var listingPriceTextField: UITextField!
+    @IBOutlet weak var quantityTextField: UITextField!
+    
+    // MARK: - IBActions
+    
+    @IBAction func saveButtonTapped(_ sender: UIBarButtonItem) {
+        // TODO
+    }
+    
     // MARK: - Lifecycle
 
     override func viewDidLoad() {
