b64b90796c939f0fa962b68d60899f892c53a5fd
Bronson Mullens <bronsonmullens@icloud.com>
2021-02-12 01:17:49 -0800

    Created model for item and a model controller.

---
+++ FlippingApp/Model Controllers/ItemController.swift
@@ -0,0 +1,17 @@
+//
+//  ItemController.swift
+//  FlippingApp
+//
+//  Created by Bronson Mullens on 2/12/21.
+//
+
+import UIKit
+
+class ItemController {
+    
+    // MARK: - Properties
+    
+    var listedItems: [Item] = []
+    var soldItems: [Item] = []
+    
+}
---
+++ FlippingApp/Models/Item.swift
@@ -0,0 +1,19 @@
+//
+//  Item.swift
+//  FlippingApp
+//
+//  Created by Bronson Mullens on 2/12/21.
+//
+
+import UIKit
+
+struct Item {
+    
+    // MARK: - Properties
+    
+    var title: String
+    var purchasePrice: Double
+    var listingPrice: Double
+    var quantity: Int
+    
+}
