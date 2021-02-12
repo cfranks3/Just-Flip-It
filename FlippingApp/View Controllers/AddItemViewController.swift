80eb6001942acab3e95c890b0ef44b6981f84651
Bronson Mullens <bronsonmullens@icloud.com>
2021-02-12 01:34:08 -0800

    Created add item vc.

---
+++ FlippingApp/View Controllers/AddItemViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AddItemViewController.swift
+//  FlippingApp
+//
+//  Created by Bronson Mullens on 2/12/21.
+//
+
+import UIKit
+
+class AddItemViewController: UIViewController {
+    
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
