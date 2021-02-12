3edf815413cef4ccb1395a4e30c433ba25adacbd
Bronson Mullens <bronsonmullens@icloud.com>
2021-02-12 01:14:42 -0800

    Set up labels in dashboard vc and hooked up IBOutlets.

--- FlippingApp/View Controllers/DashboardViewController.swift
+++ FlippingApp/View Controllers/DashboardViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class DashboardViewController: UIViewController {
     
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var profitLabel: UILabel!
+    @IBOutlet weak var inventoryValueLabel: UILabel!
+    
+    
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
