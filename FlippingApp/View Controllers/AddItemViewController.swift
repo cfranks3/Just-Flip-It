f80bd3183487133172c842dc38f6c357f24d87f1
Bronson Mullens <bronsonmullens@icloud.com>
2021-02-14 01:22:19 -0800

    Added animation and alert if user attempts to incorrectly save a new item to their inventory.

--- FlippingApp/View Controllers/AddItemViewController.swift
+++ FlippingApp/View Controllers/AddItemViewController.swift
@@ -37,12 +37,37 @@ class AddItemViewController: UIViewController {
            let quantity = quantityTextField.text,
            !quantity.isEmpty {
             let item = Item(title: title,
-                            purchasePrice: Double(purchasePrice) ?? 0,
-                            listingPrice: Double(listingPrice) ?? 0,
-                            quantity: Int(quantity) ?? 0)
+                            purchasePrice: Double(purchasePrice) ?? -1,
+                            listingPrice: Double(listingPrice) ?? -1,
+                            quantity: Int(quantity) ?? -1)
             itemController?.addListedItem(with: item)
             delegate?.itemWasAdded()
             navigationController?.popViewController(animated: true)
+        } else {
+            let alert = UIAlertController(title: "Hold on there...", message: "Are you sure you gave each text field a valid input? Check and try again.", preferredStyle: .alert)
+            let action = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+            alert.addAction(action)
+            present(alert, animated: true, completion: nil)
+            
+            UIView.animate(withDuration: 5) {
+                if self.titleTextField.text == "" {
+                    self.titleTextField.backgroundColor = .systemRed
+                    self.titleTextField.backgroundColor = .none
+                }
+                if self.purchasePriceTextField.text == "" {
+                    self.purchasePriceTextField.backgroundColor = .systemRed
+                    self.purchasePriceTextField.backgroundColor = .none
+                }
+                if self.listingPriceTextField.text == "" {
+                    self.listingPriceTextField.backgroundColor = .systemRed
+                    self.listingPriceTextField.backgroundColor = .none
+                }
+                if self.quantityTextField.text == "" {
+                    self.quantityTextField.backgroundColor = .systemRed
+                    self.quantityTextField.backgroundColor = .none
+                }
+            }
+            
         }
     }
     
